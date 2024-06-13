6cb2901e96d36fbe811800caaf777517ef75f4c4
DungVVd <dungvvd@bkav.com>
2024-06-13 16:02:05 +0700

    Vũ Văn Dũng: Chinh sua homeAdmin

--- FrontEnd/src/screen/Admin/home/HomeAdminScreen.js
+++ FrontEnd/src/screen/Admin/home/HomeAdminScreen.js
@@ -10,105 +10,14 @@ const cx = classNames.bind(styles);
 
 function HomeAdminScreen (props) {
 
-    const state1 = {
-        options: {},
-        series: [44, 55, 41, 17, 15],
-        labels: ['A', 'B', 'C', 'D', 'E']
-    }
-
-    const state2 = {
-
-        series: [{
-            name: 'series1',
-            data: [31, 40, 28, 51, 42, 109, 100]
-        }, {
-            name: 'series2',
-            data: [11, 32, 45, 32, 34, 52, 41]
-        }],
-        options: {
-            chart: {
-                height: 350,
-                type: 'area'
-            },
-            dataLabels: {
-                enabled: false
-            },
-            stroke: {
-                curve: 'smooth'
-            },
-            xaxis: {
-                type: 'datetime',
-                categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-            },
-            tooltip: {
-                x: {
-                    format: 'dd/MM/yy HH:mm'
-                },
-            },
-        },
-    };
-
-    const state3 = {
-
-        series: [{
-            name: 'Net Profit',
-            data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
-        }, {
-            name: 'Revenue',
-            data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
-        }, {
-            name: 'Free Cash Flow',
-            data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
-        }],
-        options: {
-            chart: {
-                type: 'bar',
-                height: 350
-            },
-            plotOptions: {
-                bar: {
-                    horizontal: false,
-                    columnWidth: '55%',
-                    endingShape: 'rounded'
-                },
-            },
-            dataLabels: {
-                enabled: false
-            },
-            stroke: {
-                show: true,
-                width: 2,
-                colors: ['transparent']
-            },
-            xaxis: {
-                categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
-            },
-            yaxis: {
-                title: {
-                    text: '$ (thousands)'
-                }
-            },
-            fill: {
-                opacity: 1
-            },
-            tooltip: {
-                y: {
-                    formatter: function (val) {
-                        return "$ " + val + " thousands"
-                    }
-                }
-            }
-        },
-    };
-
-    const state4 = {
-        series: [44, 55, 13, 43, 22],
+    const dataChar1 = {
+        series: [44, 55, 13, 60, 5],
         options: {
             chart: {
                 width: 380,
                 type: 'pie',
             },
-            labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+            labels: ['Iphone 15 Pro', 'Iphone 15 Pro max', 'Iphone 14 Pro', 'Iphone 12 Pro max', 'Các loại sản phẩm khác'],
             responsive: [{
                 breakpoint: 480,
                 options: {
@@ -126,6 +35,108 @@ function HomeAdminScreen (props) {
                     },
                 }
             }]
+        },
+    };
+
+    const dataChar2 = {
+        series: [44, 55, 13, 43, 22],
+        options: {
+            chart: {
+                width: 380,
+                type: 'pie',
+            },
+            labels: ["PROCESSING", "DONE", "CACELED", "WAITING", "SHIPPING"],
+            responsive: [{
+                breakpoint: 480,
+                options: {
+                    chart: {
+                        width: 200
+                    },
+                    legend: {
+                        position: 'bottom'
+                    },
+                    dataLabels: {
+                        formatter(val, opts) {
+                            const name = opts.w.globals.labels[opts.seriesIndex]
+                            return [name, val.toFixed(1) + '%']
+                        }
+                    },
+                }
+            }]
+        },
+    };
+
+    const dataChar3 = {
+
+        series: [{
+            name: 'Số lượng đã bán',
+            data: [20, 30, 40, 11, 50, 36, 32, 23, 14, 8, 5, 2]
+        }],
+        options: {
+            chart: {
+                height: 350,
+                type: 'bar',
+            },
+            plotOptions: {
+                bar: {
+                    borderRadius: 10,
+                    dataLabels: {
+                        position: 'top', // top, center, bottom
+                    },
+                }
+            },
+            dataLabels: {
+                enabled: true,
+                formatter: function (val) {
+                    return val;
+                },
+                offsetY: -20,
+                style: {
+                    fontSize: '12px',
+                    colors: ["#304758"]
+                }
+            },
+
+            xaxis: {
+                categories: ["Iphone 15", "Iphone 14", "Iphone 13", "Iphone 12", "Iphone 11", "Iphone 10", "Iphone 15 pro", "Iphone 15 pro max", "Iphone 12 pro max", "Iphone 12 pro", "Iphone 11 pro", "Khác"],
+                position: 'bottom',
+                axisBorder: {
+                    show: false
+                },
+                axisTicks: {
+                    show: false
+                },
+                crosshairs: {
+                    fill: {
+                        type: 'gradient',
+                        gradient: {
+                            colorFrom: '#D8E3F0',
+                            colorTo: '#BED1E6',
+                            stops: [0, 100],
+                            opacityFrom: 0.4,
+                            opacityTo: 0.5,
+                        }
+                    }
+                },
+                tooltip: {
+                    enabled: true,
+                }
+            },
+            yaxis: {
+                axisBorder: {
+                    show: false
+                },
+                axisTicks: {
+                    show: false,
+                },
+                labels: {
+                    show: false,
+                    formatter: function (val) {
+                        return val;
+                    }
+                }
+
+            },
         },
     };
 
@@ -274,6 +285,9 @@ function HomeAdminScreen (props) {
 
     return (
         <div className={cx('HomeAdminScreen')}>
+
+            <div className={cx('textHeader')}>Thông số bán hàng trong tuần vừa qua</div>
+
             <div className={cx('flex', 'headerA')}>
                 <div className={cx('itemH', 'flex')} style={{ color: '#c26b02' }}>
                     <div className={cx('iconH')}>
@@ -282,7 +296,7 @@ function HomeAdminScreen (props) {
 
                     <div>
                         <div>Tổng đơn hàng</div>
-                        <div>100</div>
+                        <div>1264</div>
                         <div className={cx('flex', 'center', 'percent', 'green')}>
                             <i className={cx('bx bxs-up-arrow')} style={{ marginRight: 5 }}></i>
                             <div>5% Tuần trước</div>
@@ -297,7 +311,7 @@ function HomeAdminScreen (props) {
 
                     <div>
                         <div>Tổng danh thu</div>
-                        <div>{formatPrice(10000000)}</div>
+                        <div>{formatPrice(123120000000)}</div>
                         <div className={cx('flex', 'center', 'percent', 'green')}>
                             <i className={cx('bx bxs-up-arrow')} style={{ marginRight: 5 }}></i>
                             <div>5% Tuần trước</div>
@@ -307,12 +321,12 @@ function HomeAdminScreen (props) {
 
                 <div className={cx('itemH', 'flex')} style={{ color: '#950bc7'}}>
                     <div className={cx('iconH')}>
-                        <i className='bx bx-user-plus'></i>
+                        <i className='bx bxs-bar-chart-alt-2'></i>
                     </div>
 
                     <div>
-                        <div>Tổng khách hàng</div>
-                        <div>10</div>
+                        <div>Lợi nhuận</div>
+                        <div>10 %</div>
                         <div className={cx('flex', 'center', 'percent', 'red')}>
                             <i className={cx('bx bxs-down-arrow')} style={{ marginRight: 5 }}></i>
                             <div>5% Tuần trước</div>
@@ -323,8 +337,8 @@ function HomeAdminScreen (props) {
 
             <div className={cx('listChart')}>
 
-                <div className={cx('flex', 'center', 'date')}>
-                    <div className={cx('textDay')}>Từ ngày</div>
+                <div className={cx('flex', 'center', 'textHeader')}>
+                    <div className={cx('textDay')}>Thông số bàn hàng từ ngày</div>
                     <input className={cx('inputDay')} type={'date'} value={dayFrom} onChange={handleDateFrom} />
                     <div className={cx('textDay')}>Đến ngày</div>
                     <input className={cx('inputDay')} type={'date'} value={dayTo} onChange={handleDateTo} />
@@ -332,34 +346,72 @@ function HomeAdminScreen (props) {
                     <div className={cx('btnThongKe')}>Thống kê</div>
                 </div>
 
-                <div className={cx('flex', 'warp', 'center')}>
-                    <Chart
-                        options={state1.options}
-                        series={state1.series}
-                        type="donut"
-                        width="500"
-                    />
+                <div className={cx('flex', 'headerA')}>
+                    <div className={cx('itemH', 'flex')} style={{ color: '#c26b02' }}>
+                        <div className={cx('iconH')}>
+                            <i className='bx bx-shopping-bag'></i>
+                        </div>
 
-                    <Chart
-                        options={state2.options}
-                        series={state2.series}
-                        type="area"
-                        width="800"
-                    />
+                        <div>
+                            <div>Tổng đơn hàng</div>
+                            <div>100</div>
+                        </div>
+                    </div>
 
-                    <Chart
-                        options={state3.options}
-                        series={state3.series}
-                        type="bar"
-                        width="800"
-                    />
+                    <div className={cx('itemH', 'flex')} style={{ color: '#03359a'}}>
+                        <div className={cx('iconH')}>
+                            <i className='bx bx-dollar-circle'></i>
+                        </div>
 
-                    <Chart
-                        options={state4.options}
-                        series={state4.series}
-                        type="pie"
-                        width="500"
-                    />
+                        <div>
+                            <div>Tổng danh thu</div>
+                            <div>{formatPrice(123000000)}</div>
+                        </div>
+                    </div>
+
+                    <div className={cx('itemH', 'flex')} style={{ color: '#950bc7'}}>
+                        <div className={cx('iconH')}>
+                            <i className='bx bxs-bar-chart-alt-2'></i>
+                        </div>
+
+                        <div>
+                            <div>Lợi nhuận</div>
+                            <div>20 %</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={cx('flex', 'warp', 'center', 'spaceBetween')}>
+                    <div className={cx('mg10')}>
+                        <div className={cx('mg10')}>Các loại sản phẩm đã bán (%)</div>
+                        <Chart
+                            options={dataChar1.options}
+                            series={dataChar1.series}
+                            type="pie"
+                            width="600"
+                        />
+                    </div>
+
+                    <div className={cx('mg10')}>
+                        <div className={cx('mg10')}>Trạng thái các đơn hàng (%)</div>
+                        <Chart
+                            options={dataChar2.options}
+                            series={dataChar2.series}
+                            type="donut"
+                            width="550"
+                        />
+                    </div>
+
+                    <div className={cx('mg10')}>
+                        <div className={cx('mg10')}>Số lượng sản phẩm đã bán (các sản phẩm nổi bật)</div>
+                        <Chart
+                            options={dataChar3.options}
+                            series={dataChar3.series}
+                            type="bar"
+                            width="1200"
+                        />
+                    </div>
+
                 </div>
             </div>
 
